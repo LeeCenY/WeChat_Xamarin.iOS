e71a5b32df29e9819bc8f67231b4a5627b67995e
LeeCenY <634348197@qq.com>
2018-09-28 16:29:50 +0800

    add WeChat demo

---
+++ WeChat_XamarinDemo/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using UIKit;
+using WeChat_Xamarin;
+
+namespace WeChat_XamarinDemo
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            WXSendMessage("SDK1.8.3" +
+                          "1.SDK增加调起微信刷卡支付接口" +
+                          "2.SDK增加小程序订阅消息接口" +
+                          "3.修复小程序订阅消息接口没有resp的问题");
+        }
+
+        void WXSendMessage(string text)
+        {
+            try
+            {
+                SendMessageToWXReq req = new SendMessageToWXReq();
+                req.Text = text;
+                req.BText = true;
+                req.Scene = (int)WXScene.Session;
+
+                var result = WXApi.SendReq(req);
+                UIAlertView alertView = new UIAlertView("", "分享结果:" + result, null, "取消");
+                alertView.Show();
+            }
+            catch (Exception ex)
+            {
+                UIAlertView alertView = new UIAlertView("", "错误:" + ex, null, "取消");
+                alertView.Show();
+            }
+        }
+    }
+}
